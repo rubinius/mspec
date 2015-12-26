a89d197f7436408e5595942d0ce80fcefd13171a
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-26 11:29:21 +0100

    Don't use RSpec's "double" in the lib/ directory
    Fixes #1

--- lib/mspec/helpers/mock_to_path.rb
+++ lib/mspec/helpers/mock_to_path.rb
@@ -1,6 +1,8 @@
+require 'mspec/mocks/proxy'
+
 class Object
   def mock_to_path(path)
-    obj = double('path')
+    obj = MockObject.new('path')
     obj.should_receive(:to_path).and_return(path)
     obj
   end
